69940078bdac928990c0afd3da30cb272fb651c4
ShiningSaurabh <34839782+ShiningSaurabh@users.noreply.github.com>
2018-01-12 02:06:45 +0530

    Addded day3 solution #61
    language used:C++14
    online judge: https://ideone.com/kOzfiL

---
+++ github day3.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    int p;
+    while(t--)
+    {
+        cin>>p;
+        cout<<p/2<<"\n";
+    }
+    return 0;
+}
